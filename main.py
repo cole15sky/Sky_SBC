d889b26452980138b437cad4fb02af84d1140961
Aakash <133042152+cole15sky@users.noreply.github.com>
2024-01-19 13:05:41 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+print("welcome to Sky's present 'KO BANXA CROREPATI' quiz!")
+
+playing = input("Do you want to play the game? ")
+if playing!="yes":
+    quit()
+else:
+    print("okay! Than let's play :)")
+answer = input("What's my name? ")
+if answer == "aakash":
+    print("wOW!,You have won 50 lakhs")
+else:
+    print("incorrect!")
+
+answer = input("Where am i from? ")
+if answer == "chitwan":
+    print("wOW!,You have won one crore ")
+else:
+    print("sorry you loose!")
+
+answer = input("What am i studying? ")
+if answer == "csit":
+    print("congrats again!,You have won 3 crore")
+else:
+    print("SORRRRRYYYY BOI!")
+
+answer = input("Where do` i currently live? ")
+if answer == "patan":
+    print("wOW again!,You have won jackpot money altogether; 9 crore")
+else:
+    print("incorrect!")
+answer = input("What's my fav season? ")
+if answer == "summer":
+    print("congrats you have won a worth of 18 crores. WELL DONE..")
+else:
+    print("sorry ")
